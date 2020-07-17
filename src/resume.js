4c6bb8981665f44d0a935fe75c8ba03eb72f4e02
JR Kim <jr@JRs-MacBook-Pro.local>
2020-07-17 17:04:14 -0500

    Initial Commit

---
+++ src/resume.js
@@ -0,0 +1,65 @@
+export const resume = [
+    {
+        name: 'JR Kim',
+        address: {
+            addressStreet: '8045 Metcalf Ave',
+            apartment: '326',
+            zip: '66204',
+            city: 'Overland Park',
+            state: 'KS'
+        },
+        image: '',
+        phone: '785-375-3759',
+        email: 'jsk.fni@gmail.com',
+        experience: 
+        [
+            {
+                type: 'Experience',
+                company: 'Cerner Corporation',
+                img: '',
+                location: 'Kansas City MO',
+                title: 'Software Engineer',
+                date: 'May 2017 - July 2019',
+                description: 'Design thoroughly tested healthcare software with one hundred percent test coverage.  Delivered continuous development in software for a substantial amount of products maintained by the team.',
+                duties: 
+                [
+                    'Technologies/Tools Used: .NET, C#, C++, ReactJS/Redux + Jest, Java, HTML5, CSS, GIT, Subversion, Visual Studio, VSCode, JIRA, Jenkins, Crucible.',
+                    'Multitasking and managing over twenty different solutions across five different languages at any given time based on client time constraints.',
+                    'Attended a mandatory AGILE hands on workshop to perform daily task management efficiency and effectively.',
+                    'Contributed software related development for products to meet requirements through successful deployment.  Worked side by side with the DevOps team to ensure that the Jenkins build performed successfully in order to meet time constraints during our quarterly releases.',
+                    'Quickly adhered to company and team coding/task management standards by upholding policy and team guidelines.  Achieved by using universal standards such as ESLint.',
+                    'Introduced methodologies, design, architecture, and best practices by leading new development/scripting in testing environments.  Created modular reusable tests that became team standards for environments such as Eggplant.',
+                    'Maintained and created new development for C++/C# backend, and HTML, CSS, React, .NET Forms front end.',
+                    'Provided end to end testing to ensure meaningful tests to prevent breaking code changes.  During quarterly releases, tests would catch errors where, in some cases, requirements were not being met through automated testing.  Specifically, a component overhaul in Patient Signatures where actions dispatched to Redux were not managing the state of a lookup list button properly.  It would instead open the previous modal dialog.  A snapshot of the html code provided insight on the error the overhaul created during workflow functional testing.',
+                ]
+                
+            },
+            {
+                type: 'Internship',
+                company: 'Cerner Corporation',
+                img: '',
+                location: 'Kansas City MO',
+                title: 'Software Engineer',
+                date: 'May 2017 - July 2019',
+                description: '',
+                duties: 
+                [
+                    ''
+                ]
+            },
+            {
+                type: 'Internship',
+                company: 'Cerner Corporation',
+                img: '',
+                location: 'Kansas City MO',
+                title: 'Software Engineer',
+                date: 'May 2017 - July 2019',
+                description: '',
+                duties: 
+                [
+                    ''
+                ]
+            }
+        ]
+    }
+]
