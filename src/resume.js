17154e396a79c6a5e0d9b7441e6fd5669e0c5e41
joungskim <jsk.fni@gmail.com>
2020-07-19 00:29:53 -0500

    Updated Resume.js file to include more data to use on web application temporarily.

--- src/resume.js
+++ src/resume.js
@@ -1,65 +1,154 @@
-export const resume = [
-    {
-        name: 'JR Kim',
-        address: {
-            addressStreet: '8045 Metcalf Ave',
-            apartment: '326',
-            zip: '66204',
-            city: 'Overland Park',
-            state: 'KS'
+export const resume = {
+    name: 'JR Kim',
+    image: '',
+    phone: '785-375-3759',
+    email: 'jsk.fni@gmail.com',
+    objective: 'Seeking a Software Developer position in an upward growing organization to utilize expertise in software development and testing using agile development methodologies.',
+    github: 'https://github.com/joungskim',
+    experience: [{
+            type: 'Full-Time',
+            company: 'Cerner Corporation',
+            img: '',
+            location: 'Kansas City MO',
+            title: 'Software Engineer',
+            date: 'May 2017 - July 2019',
+            description: 'Design thoroughly tested healthcare software with one hundred percent test coverage.  Delivered continuous development in software for a substantial amount of products maintained by the team.',
+            duties: [
+                'Technologies/Tools Used: .NET, C#, C++, ReactJS/Redux + Jest, Java, HTML5, CSS, GIT, Subversion, Visual Studio, VSCode, JIRA, Jenkins, Crucible.',
+                'Multitasking and managing over twenty different solutions across five different languages at any given time based on client time constraints.',
+                'Attended a mandatory AGILE hands on workshop to perform daily task management efficiency and effectively.',
+                'Contributed software related development for products to meet requirements through successful deployment.  Worked side by side with the DevOps team to ensure that the Jenkins build performed successfully in order to meet time constraints during our quarterly releases.',
+                'Quickly adhered to company and team coding/task management standards by upholding policy and team guidelines.  Achieved by using universal standards such as ESLint.',
+                'Introduced methodologies, design, architecture, and best practices by leading new development/scripting in testing environments.  Created modular reusable tests that became team standards for environments such as Eggplant.',
+                'Maintained and created new development for C++/C# backend, and HTML, CSS, React, .NET Forms front end.',
+                'Provided end to end testing to ensure meaningful tests to prevent breaking code changes.  During quarterly releases, tests would catch errors where, in some cases, requirements were not being met through automated testing.  Specifically, a component overhaul in Patient Signatures where actions dispatched to Redux were not managing the state of a lookup list button properly.  It would instead open the previous modal dialog.  A snapshot of the html code provided insight on the error the overhaul created during workflow functional testing.',
+            ]
+
         },
-        image: '',
-        phone: '785-375-3759',
-        email: 'jsk.fni@gmail.com',
-        experience: 
-        [
-            {
-                type: 'Experience',
-                company: 'Cerner Corporation',
-                img: '',
-                location: 'Kansas City MO',
-                title: 'Software Engineer',
-                date: 'May 2017 - July 2019',
-                description: 'Design thoroughly tested healthcare software with one hundred percent test coverage.  Delivered continuous development in software for a substantial amount of products maintained by the team.',
-                duties: 
-                [
-                    'Technologies/Tools Used: .NET, C#, C++, ReactJS/Redux + Jest, Java, HTML5, CSS, GIT, Subversion, Visual Studio, VSCode, JIRA, Jenkins, Crucible.',
-                    'Multitasking and managing over twenty different solutions across five different languages at any given time based on client time constraints.',
-                    'Attended a mandatory AGILE hands on workshop to perform daily task management efficiency and effectively.',
-                    'Contributed software related development for products to meet requirements through successful deployment.  Worked side by side with the DevOps team to ensure that the Jenkins build performed successfully in order to meet time constraints during our quarterly releases.',
-                    'Quickly adhered to company and team coding/task management standards by upholding policy and team guidelines.  Achieved by using universal standards such as ESLint.',
-                    'Introduced methodologies, design, architecture, and best practices by leading new development/scripting in testing environments.  Created modular reusable tests that became team standards for environments such as Eggplant.',
-                    'Maintained and created new development for C++/C# backend, and HTML, CSS, React, .NET Forms front end.',
-                    'Provided end to end testing to ensure meaningful tests to prevent breaking code changes.  During quarterly releases, tests would catch errors where, in some cases, requirements were not being met through automated testing.  Specifically, a component overhaul in Patient Signatures where actions dispatched to Redux were not managing the state of a lookup list button properly.  It would instead open the previous modal dialog.  A snapshot of the html code provided insight on the error the overhaul created during workflow functional testing.',
-                ]
-                
-            },
-            {
-                type: 'Internship',
-                company: 'Cerner Corporation',
-                img: '',
-                location: 'Kansas City MO',
-                title: 'Software Engineer',
-                date: 'May 2017 - July 2019',
-                description: '',
-                duties: 
-                [
-                    ''
-                ]
-            },
-            {
-                type: 'Internship',
-                company: 'Cerner Corporation',
-                img: '',
-                location: 'Kansas City MO',
-                title: 'Software Engineer',
-                date: 'May 2017 - July 2019',
-                description: '',
-                duties: 
-                [
-                    ''
-                ]
-            }
-        ]
-    }
-]
+        {
+            type: 'Internship',
+            company: 'Cerner Corporation',
+            img: '',
+            location: 'Kansas City MO',
+            title: 'Software Engineer',
+            date: 'May 2017 - July 2019',
+            description: 'Uplifted a Contracts web application from ASP.NET Forms to ASP.NET MVC that displays contracts through browsers for clients to review. ',
+            duties: [
+                ''
+            ]
+        },
+        {
+            type: 'Internship',
+            company: 'Cerner Corporation',
+            img: '',
+            location: 'Kansas City MO',
+            title: 'Software Engineer',
+            date: 'May 2017 - July 2019',
+            description: 'Contributed modular revisions to HCI Indexing in .NET Windows Forms Application.',
+            duties: [
+                ''
+            ]
+        },
+        {
+            type: 'CO-OP',
+            company: 'GE Aviation',
+            img: '',
+            location: 'Kansas City MO',
+            title: 'Software Engineer',
+            date: 'MAY 2014 - DECEMBER 2014',
+            description: 'Wrote tests in Visual Basic legacy code for aircraft flight software on government planes.',
+            duties: [
+                ''
+            ]
+        }
+    ],
+    technologies: [
+        'Javascript',
+        'Node.JS',
+        'ReactJS + Redux + Jest + npm',
+        'HTML5 + CSS',
+        'Java',
+        'C++',
+        'GIT + GitHub + Subversion',
+        'C# + NUnit Test + Linq',
+        'ASP.NET MVC',
+        'Microsoft SQL + MySQL + SQL'
+    ],
+    skills: [
+        'JIRA - Story and task management.',
+        'AGILE - Scrum, interval sprints, story pointing, continuous integration, design, task breakdown.',
+        'JENKINS - Build and test software safely to deploy prod to new or existing software.',
+        'RQM/EP - Full functional automated front end image detection testing software.  Eliminates manual testing for test engineers/analysts.',
+    ],
+    education: {
+        name: 'Kansas State University',
+        certification: 'Bachelors in Computer Science',
+        date: 'AUGUST 2012 - DECEMBER 2016'
+    },
+    other: [
+        'KSU Open House Presenter - Presented a built from scratch game engine built in C++ using DirectX to render a 3D Pest Simulator for American Pest Management company.',
+        'Content Creator - Streaming Platforms.',
+        'Dance - Dance team during college.',
+        'Fitness - Weight lifting for health.'
+    ],
+    citizenship: 'U.S Citizen',
+    projects: [{
+            name: 'J&S Real-Estate Web Application',
+            url: 'https://joungskim.github.io/jns-webapp/',
+            description: 'Rental properties web application inspired by brother in laws real estate side business.',
+            features: 'Single Page Application, Post New Rentals Availability, Messaging, Scheduling, Payments, GPS Mapping, virtual tour',
+            status: 'In Progress.',
+            stack: 'React, Node.js + Express.js, Javascript, Postgres, Bootstrap'
+        },
+        {
+            name: 'My Portfolio',
+            url: 'https://github.com/joungskim/my-portfolio',
+            description: 'Portfolio website to keep up with my resume, content creation, and latest projects.',
+            features: 'Single Page Application, Contact Me',
+            status: 'In Progress.',
+            stack: 'React, Node.js + Express.js, Javascript, Postgres, Bootstrap'
+        },
+        {
+            name: 'Robofriends',
+            url: 'https://joungskim.github.io/robofriends/',
+            description: 'React app that generates random robots and search functions using React.',
+            features: 'Search, Robot Generation',
+            status: 'Complete',
+            stack: 'React, Javascript, Bootstrap'
+        },
+        {
+            name: 'Secretary',
+            url: 'https://github.com/joungskim/Secretary',
+            description: 'Secretary program to create logs and clock hours worked on a specific project with notes added per day.  Built for Cerner internship to track my work each day.',
+            features: 'Tracks times for clocking in and clocking out, gives clock out time/countdown, persistent save notes tracker',
+            status: 'Complete',
+            stack: '.NET, C#, Windows Forms Application.'
+        },
+        {
+            name: 'Barsistant',
+            url: 'https://github.com/joungskim/CIS598',
+            description: 'Final Senior Project.  Bartender social media website.  Calculates cost of drinks based on user data.',
+            Features: 'Live chat, messaging, drink databases, Accounts, login/security OAuth, drink reccomendations. Over 250 hours went into this senior project.  Received 100% on grade.',
+            status: 'Complete',
+            stack: 'ASP.NET MVC 4, MSSQL, Razor, Javascript, SQL, C#, '
+        },
+        {
+            name: 'League of Legends Helper',
+            url: 'https://github.com/joungskim/mystuff',
+            description: 'Game collection of timers and characters browser linking to guides.',
+            features: 'Blue, Red, Baron, Dragon spawn timers.  Collection of all characters and links to their guides and counters.  Browser built into the forms application.',
+            status: 'Complete',
+            stack: '.NET, C#, Windows Forms Application.'
+        },
+        {
+            name: 'Card Database Project',
+            url: 'https://github.com/joungskim/CGProject',
+            description: 'Databases CIS course final project.  Wrote a card database for deck building and card information. Received 100% on grade.',
+            features: 'Live chat, card database, images of cards',
+            status: 'Complete',
+            stack: '.NET, C#, Windows Form Application, MySQL'
+        }
+    ]
+}
+
+export default resume
