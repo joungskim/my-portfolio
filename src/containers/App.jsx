8782b505dafa0283ac2467b59789816adc9df5a6
joungskim <jsk.fni@gmail.com>
2020-07-21 01:55:32 -0500

    Adding Animation package

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import 'tachyons';
-import MPHeader from '../components/MPHeader.jsx'
+import MPHeader from '../components/MPHeader.jsx';
 
 
 function App() {
     return (
         <div className="App" >
             <MPHeader />
+            <h1 class="animate__animated animate__fadeInDown">An animated element</h1>
         </div>
     );
 }
