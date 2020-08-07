b63c8eac7aba91afb0ff93214262430340f92ecc
joungskim <jsk.fni@gmail.com>
2020-08-07 12:31:40 -0500

    Updated grammar error in PostContactMe Page.

--- src/components/PostContactMe.jsx
+++ src/components/PostContactMe.jsx
@@ -5,7 +5,7 @@ const PostContactMe = () => {
     return (
         <div>
             <h1 className='tc'>{'Thanks For Contacting Me!'}</h1>
-            <p className='fw2 tc f4'><i>{'I look forward to hearing from you! I will be in touch back as soon as possible!'}</i></p>
+            <p className='fw2 tc f4'><i>{'I look forward to hearing from you! I will be in touch as soon as possible!'}</i></p>
             <p className='fw2 tc f4'><i>{'- JR Kim'}</i></p>
         </div>
     )
